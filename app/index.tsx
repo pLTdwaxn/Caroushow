67ca8b687e1fa59023f385a83b331d9c9fbe6cf1
MustachedDragon <16360973+MustachedDragon@users.noreply.github.com>
2024-10-18 02:49:50 +1030

    [#6] Show a fixture image on the index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,65 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View, StyleSheet } from "react-native";
+
+import { Image } from "expo-image";
+import * as ImagePicker from "expo-image-picker";
+
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
+
+const SocialMedia = [
+  {
+    name: "Instagram",
+    imgRatio: {
+      width: 4,
+      height: 5,
+    },
+  },
+];
 
 export default function Index() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const targetSocialMedia = SocialMedia.find(
+    (media) => media.name === "Instagram"
+  );
+
+  const imgRatio = targetSocialMedia
+    ? targetSocialMedia.imgRatio
+    : { width: 1, height: 1 };
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} imgRatio={imgRatio} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    borderWidth: 0,
+  },
+  imageContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    borderWidth: 0,
+  },
+});
---
+++ components/SocialMediaPicker.tsx
@@ -0,0 +1,5 @@
+import { Pressable } from "react-native";
+
+export default function SocialMediaPicker() {
+  return <Pressable onPress={() => {}}></Pressable>;
+}
