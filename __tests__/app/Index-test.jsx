25e28bbff33ccd1f6c5e732cfac0c69e4beee2f2
MustachedDragon <16360973+MustachedDragon@users.noreply.github.com>
2024-10-19 07:09:02 +1030

    [#15] Add jest-expo library and test for Index component

---
+++ __tests__/app/Index-test.jsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native';
+import Index from '@/app/index';
+
+describe('<Index />', () => {
+  test('Text renders correctly on HomeScreen', () => {
+    const { getByText } = render(<Index />);
+    expect(getByText('Select an image')).toBeTruthy();
+  });
+});
