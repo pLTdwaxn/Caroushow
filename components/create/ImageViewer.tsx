3da03a597c94d0ee1c1aa1012903aaefa205fa56
MustachedDragon <16360973+MustachedDragon@users.noreply.github.com>
2024-10-24 20:59:49 +1030

    [#39] Remove content fit press toggle

--- components/create/ImageViewer.tsx
+++ components/create/ImageViewer.tsx
@@ -1,27 +1,12 @@
-import { useState } from "react";
-import { StyleSheet, Pressable } from "react-native";
-import { Image, ImageContentFit } from "expo-image";
+import { StyleSheet } from "react-native";
+import { Image } from "expo-image";
 
 type Props = {
   imgSource: string | null;
 };
 
 export default function ImageViewer({ imgSource }: Props) {
-  const [contentFit, setContentFit] = useState<ImageContentFit>("contain");
-
-  const switchContentFit = () => {
-    setContentFit(contentFit === "contain" ? "cover" : "contain");
-  };
-
-  return (
-    <Pressable onPress={switchContentFit}>
-      <Image
-        source={imgSource}
-        style={styles.imageStyle}
-        contentFit={contentFit}
-      />
-    </Pressable>
-  );
+  return <Image source={imgSource} style={styles.imageStyle} />;
 }
 
 const styles = StyleSheet.create({
