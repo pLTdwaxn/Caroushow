f4bf9394eaaa18c13a3d955b1d1e6ec2ee298ab6
MustachedDragon <16360973+MustachedDragon@users.noreply.github.com>
2024-11-18 18:50:35 +1030

    Fixed issue with crop guide not showing up on image load

--- components/create/ImageViewer.tsx
+++ components/create/ImageViewer.tsx
@@ -1,9 +1,7 @@
+import { useState } from "react";
 import { connect } from "react-redux";
-
-import { StyleSheet } from "react-native";
-
+import { StyleSheet, View } from "react-native";
 import { Image } from "expo-image";
-
 import CropGuide from "@/components/create/CropGuide";
 import ImageSelectButton from "@/components/create/ImageSelectButton";
 
@@ -12,19 +10,41 @@ type Props = {
 };
 
 const ImageViewer = ({ uri }: Props) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return uri ? (
-    <Image source={{ uri: uri }} style={styles.imageStyle} contentFit="contain">
-      <CropGuide />
-    </Image>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: uri }}
+        style={styles.imageStyle}
+        contentFit="contain"
+        onLoad={() => setImageLoaded(true)}
+      />
+      {imageLoaded && (
+        <View style={styles.cropGuideContainer}>
+          <CropGuide />
+        </View>
+      )}
+    </View>
   ) : (
     <ImageSelectButton />
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   imageStyle: {
     width: "100%",
     height: "100%",
+  },
+  cropGuideContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
 
