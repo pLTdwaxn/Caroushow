e6575f224a12495e484e6b57836a8cf092b7f6fc
Tele Kinesis <dontforgit@outlook.com>
2025-02-27 23:50:16 +1030

    Added a button to reset the image selected

--- app/(tabs)/createTab/ActionsBar.tsx
+++ app/(tabs)/createTab/ActionsBar.tsx
@@ -1,15 +1,25 @@
+import { View, StyleSheet } from "react-native";
+
 import Ionicons from "@expo/vector-icons/Ionicons";
-import IconButton from "../../../components/createTab/IconButton";
-import { sliceImage } from "../../../components/createTab/utils";
-import { View, Text, StyleSheet } from "react-native";
+
+import IconButton from "@/components/createTab/IconButton";
+import { sliceImage } from "@/components/createTab/utils";
+
+import { resetImage } from "@/store/slices/imageSlice";
+import store from "@/store";
 
 const ActionsBar = () => {
   const RunIcon = <Ionicons name="download-outline" size={24} />;
+  const ResetIcon = <Ionicons name="refresh-outline" size={24} />;
+
+  const handleReset = () => {
+    store.dispatch(resetImage());
+  };
 
   return (
     <View style={styles.actionsBar} testID="actions-bar">
-      <View style={styles.IconsRow}>
-        <Text>Actions</Text>
+      <View style={styles.iconsRow}>
+        <IconButton icon={ResetIcon} onPress={handleReset} />
         <IconButton icon={RunIcon} onPress={sliceImage} />
       </View>
     </View>
@@ -19,9 +29,9 @@ const ActionsBar = () => {
 const styles = StyleSheet.create({
   actionsBar: {
     flex: 0.1,
-    margin: 12,
+    padding: 12,
   },
-  IconsRow: {
+  iconsRow: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
