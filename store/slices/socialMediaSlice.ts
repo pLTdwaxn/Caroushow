4f423a6d62e7ee18038d66d3323bcd001810f482
Tele Kinesis <dontforgit@outlook.com>
2025-03-09 02:41:56 +1030

    WIP

---
+++ store/slices/socialMediaSlice.ts
@@ -0,0 +1,22 @@
+import { socialMediaState } from "@/types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import socialMediaList from "@/fixtures/socialMediaList";
+
+const initialState: socialMediaState = {
+  name: socialMediaList[0].name,
+  minRatio: socialMediaList[0].minRatio,
+  maxRatio: socialMediaList[0].maxRatio,
+};
+
+const socialMediaSlice = createSlice({
+  name: "socialMedia",
+  initialState,
+  reducers: {
+    setSocialMedia: (state, action: PayloadAction<socialMediaState>) => {
+      state.name = action.payload.name;
+    },
+  },
+});
+
+export const { setSocialMedia } = socialMediaSlice.actions;
+export default socialMediaSlice.reducer;
