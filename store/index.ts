0e0eb788abf436c9ae03dff4020465ea376ba349
Tele Kinesis <dontforgit@outlook.com>
2025-03-09 02:46:46 +1030

    Limit drag handle movements

--- store/index.ts
+++ store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import imageReducer from "./slices/imageSlice";
 import paramReducer from "./slices/paramSlice";
 import deviceReducer from "./slices/deviceSlice";
+import socialMediaReducer from "./slices/socialMediaSlice";
 
 const store = configureStore({
   reducer: {
     device: deviceReducer,
     image: imageReducer,
     param: paramReducer,
+    socialMedia: socialMediaReducer,
   },
 });
 
